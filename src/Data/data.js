261e0f44cd5a33f356385ba20836531044580f70
Mehar Ali <mehar0alii@gmail.com>
2024-01-16 16:35:58 +0500

    first commit

---
+++ src/Data/data.js
@@ -0,0 +1,12 @@
+export const data = [
+    { title: "Eternal Embrace", desc: "In the dance of life, I want you as my forever partner. Will you be my one and only?" },
+    { title: "Heart's Symphony", desc: "Our love is a melody waiting to be played. Will you compose the rest of our song with me?" },
+    { title: "Starlit Promises", desc: "Under the vast canvas of the night sky, I confess my love. Will you be the constellation in my universe?" },
+    { title: "Love's Tapestry", desc: "Life is a beautiful tapestry, and I want you to be the vibrant thread that intertwines with mine. Will you weave this story with me?" },
+    { title: "Whispers of the Heart", desc: "In the silence between heartbeats, my love for you speaks volumes. Will you listen and become the echo in my soul?" },
+    { title: "Sunset Serenity", desc: "As the sun sets, casting hues of warmth, I realize my world is brighter with you. Will you be the sunlight in my every dawn?" },
+    { title: "Blossoming Affection", desc: "Love is a delicate flower, and my heart is the garden. Will you allow our love to bloom and flourish?" },
+    { title: "Ocean of Emotions", desc: "Just as the ocean meets the shore, my love for you knows no bounds. Will you be the tide that sweeps me away into a lifetime of joy?" },
+    { title: "Infinite Constancy", desc: "In the vastness of time, my love for you remains constant. Will you be my anchor in this ever-changing world?" },
+    { title: "Enchanted Promises", desc: "In the enchanting realm of love, I make a promise to cherish and adore you. Will you embark on this magical journey with me?" },
+  ];
---
+++ src/components/Envelope.jsx
@@ -0,0 +1,70 @@
+"use client";
+import React, { useState } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { data } from "@/Data/data";
+import { kalam, mulish } from "@/app/fonts";
+
+const Envelope = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
+    const handleClick = () => {
+      setIsOpen(!isOpen);
+    };
+  
+  
+    const prevCard = () => {
+      setCurrentIndex((prevIndex) => (prevIndex === 0 ? data.length - 1 : prevIndex - 1));
+    };
+  
+    const nextCard = () => {
+      setCurrentIndex((prevIndex) => (prevIndex === data.length - 1 ? 0 : prevIndex + 1));
+    };
+
+  return (
+    <div className="w-full h-screen flex justify-center items-end pb-28 relative bg-rose-900">
+      {/* Complete Enelope */}
+      <div className="w-80 h-60 relative">
+        {/* Evelope 4 pages */}
+        <div
+          className="w-80 h-60 ring-4 ring-rose-800 overflow-hidden cursor-pointer  rounded-xl absolute"
+          onClick={handleClick}
+        >
+          <div className="w-60 h-80 -right-[185px] absolute ring-2 ring-rose-800 -rotate-45 bg-white z-10 bg-gradient-to-l from-pink-600 via-pink-700 to-red-800"></div>
+          <div className="w-60 h-80 -left-[185px] absolute ring-2 ring-rose-800 rotate-45 bg-white z-10 bg-gradient-to-r from-pink-600 via-pink-700 to-red-800"></div>
+          <div className="w-60 h-80 left-3 -bottom-[185px] rounded-t-3xl absolute ring-2 ring-rose-800 rotate-45 z-10 bg-white bg-gradient-to-tr from-pink-600 via-pink-700 to-red-800"></div>
+          <div
+            className={`w-60 h-80 right-3 -top-[195px] rounded-b-3xl absolute ring-2 ring-rose-800 transition-all ease-out delay-300 bg-gradient-to-b from-pink-600 via-pink-700 to-red-800 ${
+              isOpen ? "-z-40" : "rotate-45"
+            } z-10 bg-white`}
+          ></div>
+        </div>
+        {/* Evelope Cards */}
+        <div
+          className={`w-full h-full absolute bg-gradient-to-br from-red-900 via-rose-800 to-red-900 ring-2 ring-white z-0 transition-all ease-out delay-300 rounded-xl p-1 text-white ${
+            isOpen ? "-translate-y-32" : "translate-y-0"
+          }`}
+        >
+          <MdChevronLeft
+            size={20}
+            className="bg-white left-1 cursor-pointer rounded-full absolute opacity-50 hover:opacity-100 z-50 text-black top-1/2 transform -translate-y-1/2"
+            onClick={prevCard}
+          />
+          <div className="w-full h-full border-2 border-white border-dashed rounded-lg p-5">
+            <div className="w-full h-full flex flex-col text-white text-center space-y-2">
+              <h1 className={`font-semibold text-lg ${mulish.className}`}>{data[currentIndex].title}</h1>
+              <p className={`${kalam.className}`}>{data[currentIndex].desc}</p>
+            </div>
+          </div>
+          <MdChevronRight
+            size={20}
+            className="bg-white right-1 cursor-pointer rounded-full absolute opacity-50 hover:opacity-100 z-50 text-black top-1/2 transform -translate-y-1/2"
+            onClick={nextCard}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Envelope;
